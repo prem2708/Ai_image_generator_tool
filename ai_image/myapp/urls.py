0746df168e8033f5a8a60bbf7d3a79a6c8a86483
prem2708 <preminnovator2708@gmail.com>
2025-06-19 12:24:02 +0530

    done

---
+++ ai_image/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register/', views.register_view, name="register"),
+    path('login/', views.login_view, name="login"),
+    path('ai_image/', views.ai_image, name="ai_image"),
+    path('logout/', views.logout_view, name="logout"),
+]
+
+    #const MODEL_URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0`;
